90db1759dcdaeab41f348c14938746ffada7d87b
caioAugustoRodrigues <caio.turmaxii@gmail.com>
2020-01-17 15:55:09 -0300

    modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/assets/2.png 	new file:   src/assets/img/moon.jpg 	new file:   src/assets/img/satelite-inner.jpg 	new file:   src/assets/img/satelite.jpg 	new file:   src/assets/joyjet.png 	new file:   src/assets/joyjet.svg 	new file:   src/components/hero/index.jsx 	new file:   src/components/navbar/index.jsx 	new file:   src/css/main.css 	new file:   src/css/main.css.map 	new file:   src/desktop.ini 	modified:   src/logo.svg 	new file:   src/scss/_config.scss 	new file:   src/scss/_partials.scss 	new file:   src/scss/_resets.scss 	new file:   src/scss/components/_hero.scss 	new file:   src/scss/components/_logo.scss 	new file:   src/scss/components/_navbar.scss 	new file:   src/scss/main.scss

---
+++ src/components/hero/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero__text">
+                <h1>Space<span>.</span></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie elit at lacus…</p>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+export default function NavBar() {
+    
+    let showMenu = false;
+    
+    function toggleMenu() {
+        const hamburger = document.querySelector('.menu-btn__burger');
+        const nav = document.querySelector('.nav');
+        const menuNav = document.querySelector('.menu-nav');
+        const navItems = document.querySelectorAll('.menu-nav__item');
+
+        if (!showMenu) {
+            hamburger.classList.add('open');
+            nav.classList.add('open');
+            menuNav.classList.add('open');
+            navItems.forEach(item => item.classList.add('open'));
+
+            showMenu = true;
+        } else {
+            hamburger.classList.remove('open');
+            nav.classList.remove('open');
+            menuNav.classList.remove('open');
+            navItems.forEach(item => item.classList.remove('open'));
+
+            showMenu = false;
+        }
+    }
+
+    return (
+        <header>
+            <span className="logo"></span>
+
+            <div className="menu-btn" onClick={toggleMenu}>
+                <span className="menu-btn__burger"></span>
+            </div>
+
+            <nav className="nav">
+                <div className="menu-nav">
+                    <ul className="menu-nav__inner">
+                        <li className="menu-nav__item">
+                            <a href="#" className="menu-nav__link">
+                                Blog
+                            </a>
+                        </li>
+                        <li className="menu-nav__item">
+                            <a href="#" className="menu-nav__link">
+                                Popular
+                            </a>
+                        </li>
+                        <li className="menu-nav__item contact">
+                            <a href="#" className="menu-nav__link">
+                                Archive
+                            </a>
+                        </li>
+                        <li className="menu-nav__item contact">
+                            <a href="#" className="menu-nav__link">
+                                About
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+}
