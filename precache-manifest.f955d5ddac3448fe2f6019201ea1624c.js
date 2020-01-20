13e83e313fe16b245a52bb287ad54838f9b00781
caioAugustoRodrigues <caio.turmaxii@gmail.com>
2020-01-20 17:15:55 -0300

    Updates

---
+++ precache-manifest.f955d5ddac3448fe2f6019201ea1624c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea9ce823fa85017bdac2d8ceeb92abb",
+    "url": "/Joyjet-WebDesigner/index.html"
+  },
+  {
+    "revision": "f48d3bbd18ed71e026ed",
+    "url": "/Joyjet-WebDesigner/static/css/main.ba7bcbf8.chunk.css"
+  },
+  {
+    "revision": "62052aba3d30a4e1c5c6",
+    "url": "/Joyjet-WebDesigner/static/js/2.95feaf07.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Joyjet-WebDesigner/static/js/2.95feaf07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f48d3bbd18ed71e026ed",
+    "url": "/Joyjet-WebDesigner/static/js/main.7a5b83f1.chunk.js"
+  },
+  {
+    "revision": "b246f9dc6e8f66e616b0",
+    "url": "/Joyjet-WebDesigner/static/js/runtime-main.8c0cd006.js"
+  },
+  {
+    "revision": "e2480c1491d3f1dbf5b682210d262c27",
+    "url": "/Joyjet-WebDesigner/static/media/joyjet.e2480c14.png"
+  },
+  {
+    "revision": "9cefec53387add5a1e825fcb7e7e85cf",
+    "url": "/Joyjet-WebDesigner/static/media/moon.9cefec53.jpg"
+  },
+  {
+    "revision": "1e2e6cdb4c8d17375a381b02f65b7f0d",
+    "url": "/Joyjet-WebDesigner/static/media/satellite-inner.1e2e6cdb.jpg"
+  },
+  {
+    "revision": "e424a9defdb868435ecc8b3c020d51a4",
+    "url": "/Joyjet-WebDesigner/static/media/satellite.e424a9de.jpg"
+  },
+  {
+    "revision": "d55a2c5cdc120c2a8fd2969dde256143",
+    "url": "/Joyjet-WebDesigner/static/media/satelliteb.d55a2c5c.jpg"
+  }
+]);
